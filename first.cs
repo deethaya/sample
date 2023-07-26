e60ba9301f9c40dc857d208a53da524c06eb16e3
deethaya <140626442+deethaya@users.noreply.github.com>
2023-07-26 19:40:49 +0530

    Create first.cs

---
+++ first.cs
@@ -0,0 +1,3 @@
+hi this was first file we created in git hub project 
+we have projeact one in line the create the time of project to bring 
+the value of project to will bring the motive of project the code was runing the vallu 
